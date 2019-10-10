b45ec979429d03eaabf400408815f36f59224ea6
Emil Fjellström <emil.fjellstrom@makingwaves.com>
2019-10-10 20:21:45 +0200

    Meta and stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Billhop Rövarspråket`,
+    description: `Assignment to create a rövarspråk translator`,
+    author: `Emil F.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-sass`,
     {
